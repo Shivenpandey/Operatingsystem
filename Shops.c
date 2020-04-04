16712b8443a8e68e110769a08858e8f033d0e21d
Shivenpandey <54632727+Shivenpandey@users.noreply.github.com>
2020-04-04 08:57:10 +0530

    Update Shops.c

--- Shops.c
+++ Shops.c
@@ -2,7 +2,7 @@
 There are lots of gift shops, they all are going to the gift shops and 
 randomly picking the gifts. After picking the gifts they are randomly 
 arriving in the billing counter. The accountant gives the preference to 
-that student who has maximum number of gifts. 
+that student who has maximum number of gift. 
 Create a C or Java program to define order of billed students?*/
 #include <stdio.h> 
 
